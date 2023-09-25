279802d436eac264d1bf778d7a6219adb996b22b
Clay <claytongall46@gmail.com>
2023-09-24 19:30:39 -0500

    Add CountViewModel

---
+++ app/src/main/java/com/gall/msu/hw4courtcounter/CountViewModel.kt
@@ -0,0 +1,17 @@
+package com.gall.msu.hw4courtcounter
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "QuizViewModel"
+const val CURRENT_SCORE_A = "CURRENT_SCORE_A"
+const val CURRENT_SCORE_B = "CURRENT_SCORE_B"
+
+class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+    var scoreTeamA: Int
+        get() = savedStateHandle.get(CURRENT_SCORE_A) ?: 0
+        set(value) = savedStateHandle.set(CURRENT_SCORE_A, value)
+    var scoreTeamB: Int
+        get() = savedStateHandle.get(CURRENT_SCORE_B) ?: 0
+        set(value) = savedStateHandle.set(CURRENT_SCORE_B, value)
+}
